5f3fabf7661484927294e74ba3dabdd0c1d04c12
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-12-20 08:16:34 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var el = document.createElement('div');
+el.innerHTML =  'Hello, DOM!';
+el.style.backgroundColor = '#f4f4f4';
+
+document.body.appendChild(el);
+el.style.textAlign = 'center';
+
+var ul = document.createElement('ul');
+
+for(let i=0; i<5; i++){
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+el.appendChild(ul);
+ul.style.textAlign = 'left';
